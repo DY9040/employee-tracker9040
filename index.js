7d56caf9db2b8eba084e8aae497ee112967970df
Forever Young <dy9040@gmail.com>
2022-06-11 05:10:15 -0500

    updated employee rols in index.js

--- index.js
+++ index.js
@@ -68,7 +68,10 @@ const viewDepartments = () => {
 };
 
 const viewRoles = () =>{
-    db.query('SELECT * FROM roles', function (err, results) {
+    const SQL = `SELECT title, department_id, department_name, salary FROM roles
+    LEFT JOIN department ON roles.department_id = department.id`;
+
+    db.query(SQL, function (err, results) {
         console.table(results)
         return questions();
     });
@@ -78,8 +81,12 @@ const viewRoles = () =>{
 };
 
 const viewEmployees = () => {
+    const SQL = `SELECT employee.id, first_name, last_name, department_name, salary, manager_id FROM employee
+    LEFT JOIN roles ON employee.role_id = roles.id
+    LEFT JOIN department ON roles.department_id = department.id`;
 
-    db.query('SELECT * FROM employee', function (err, results) {
+
+    db.query(SQL, function (err, results) {
         console.table(results)
         return questions();
     });
@@ -89,37 +96,183 @@ const viewEmployees = () => {
 };
 
 const addDepartment = () => {
+    inquirer.prompt(
+        {
+            type: 'input',
+            name: 'departmentInput',
+            message: 'What is the name of the department?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                }else {
+                    console.log('Please enter a department!')
+                    return false;
+                }
+            }
+        }
+    )
+    .then(({ departmentInput }) => {
+        const SQL = `INSERT INTO department (department_name) VALUES (?)`;
+        const params = [departmentInput];
       //query the databases////
-      db.query(`INSERT INTO department (department_name)
-      VALUES ('Technical')`, function (err, results) {
+        db.query(SQL, params, function (err, results) {
           return questions();
       });
   
       app.use((req, res) => {
           res.status(404).end();
       });
+    });
 };
 
 const addRole = () => {
-    //query the databases////
-    db.query(`INSERT INTO roles (title, salary, department_id)
-    VALUES ('Purchaser, 30000 2')`, function (err, results) {
+    inquirer.prompt(
+        [{
+            type: 'input',
+            name: 'titleInput',
+            message: 'What is the title of the role?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                }else {
+                    console.log('Please enter a title!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'salaryInput',
+            message: 'What is the salary of the role?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                }else {
+                    console.log('Please enter a salary!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'departmentInput',
+            message: 'What is the department of the role?',
+            choices: ['1', '2', '3', '4', '5']
+        }]
+    )
+    .then(({ titleInput, salaryInput, departmentList }) => {
+
+        const SQL = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+        const params = [titleInput, salaryInput, departmentList];
+        //query the databases////
+        db.query(SQL, params, function (err, results) {
         return questions();
     });
 
     app.use((req, res) => {
         res.status(404).end();
+    });
     });
 };
 
 const addEmployee = () => {
+    inquirer.prompt(
+        [{
+            type: 'input',
+            name: 'firstNameInput',
+            message: 'What is the first name of the employee?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                }else {
+                    console.log('Please enter a first name!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastNameInput',
+            message: 'What is the last name of the employee?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                }else {
+                    console.log('Please enter a last name!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'roleInput',
+            message: 'What is the role of the employee?',
+            choices: ['1', '2', '3']
+        },
+        {
+            type: 'list',
+            name: 'managerInput',
+            message: 'Who is the manager of the employee?',
+            choices: ['1', '2', '3']
+        }]
+    )
+    .then(({ firstNameInput, lastNameInput, roleInput, managerInput }) => {
+
+        const employeeSQL = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        const params = [firstNameInput, lastNameInput, roleInput, managerInput];
+
     //query the databases////
-    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES ('David', 'Young', 3, 2)`, function (err, results) {
+   db.query(employeeSQL, params, function (err, results) {
         return questions();
     });
 
     app.use((req, res) => {
         res.status(404).end();
     });
+    });
 };
+
+const updateEmployeeRole = () => {
+    var rSQL = `SELECT * FROM employee`;
+    db.query(rSQL, (err, response) => {
+        var employeeList = [];
+        response.forEach((employee) => {employeeList.push(`${employee.first_name} ${employee.last_name}`);});
+
+        inquirer.prompt(
+            [{
+                type: 'list',
+                name: 'employeeInput',
+                message: 'Which employee would you like to update?',
+                choices: employeeList
+            },
+            {
+                type: 'list',
+                name: 'roleUpdate',
+                message: 'What is the new role of the employee?',
+                choices: ['1', '2', '3']
+            }]
+        )
+        .then(({ employeeInput, roleUpdate }) => {
+            var employeeId;
+            response.forEach((employee) => {
+                if (
+                    employeeInput === `${employee.first_name} ${employee.last_name}`
+                ) {
+                    employeeId = employee.id;
+                }
+            });
+            const SQL = `UPDATE employee SET role_id = ? WHERE id = ?`;
+            const params = [roleUpdate, employeeId];
+            db.query(SQL, params, function (err, results) {
+                return questions();
+            }
+            );
+            app.use((req, res) => {
+                res.status(404).end();
+            }
+            );
+        }
+        );
+    }
+    );
+}
